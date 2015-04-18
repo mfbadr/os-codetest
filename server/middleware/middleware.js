80cfc6f8642a7c43dbd0a78fcc471f41b983837f
Mikey Badr <mfbadr@gmail.com>
2015-04-18 16:41:28 -0400

    works with express 4, seperated routes and middleware

---
+++ server/middleware/middleware.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var morgan         = require('morgan'),
+    bodyParser     = require('body-parser'),
+    methodOverride = require('method-override'),
+    cookieParser   = require('cookie-parser'),
+    session        = require('express-session'),
+    serveStatic    = require('serve-static'),
+    redis          = require('redis'),
+    redisClient    = redis.createClient(),
+    RedisStore     = require('connect-redis')(session);
+    //security       = require('../lib/security'),
+    //users          = require('../controllers/users');
+
+module.exports = function(app, express){
+  app.use(morgan('dev'));
+  app.use(serveStatic(__dirname + '/../../public'));
+  app.use(bodyParser.urlencoded({extended:true}));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+  app.use(cookieParser());
+  //console.log('REDISCLIENTS>>>>>>>', redisClient);
+  //MAKE SURE THE REDIS DAEMON IS RUNNING!
+  // $redis-server in the terminal
+  redisClient.on('error', function(err){
+    console.log('REDISCLIENT ERROR >>>>>>', err);
+  });
+  app.use(session(
+    {
+      store:new RedisStore({client:redisClient}),
+      secret:'my super secret key',
+      resave:true,
+      saveUninitialized:true,
+      cookie:{maxAge:null}
+    }
+  ));
+  console.log('Express: Middleware Loaded');
+};
